5dba6da003b03d096a99ec534b326b78ce570a3a
mohammadabouei <a.mohammad.abouei@gmail.com>
2018-12-08 15:27:55 +0330

    Helicopter class added

---
+++ src/Helicopter.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Helicopter extends Vehicle {
+
+
+
+    private static final int UPGRADECOST = 100;
+
+    @Override
+    public int getUpgradeCost(){
+
+            return UPGRADECOST * (int) Math.pow(2,level)  + 200 - 50 * (level);
+    }
+
+    @Override
+    public  int getProductsPrice(){
+        int price = 0;
+        for (ProductType content : contents) {
+
+           price += content.getBuyCost();
+        }
+        return price;
+    }
+
+
+    public ArrayList<Product> receiveProducts(){
+
+        ArrayList<Product> receive = new ArrayList<>();
+        for (ProductType content : contents) {
+            Product temp  = new Product(content);
+            receive.add(temp);
+        }
+
+        return receive;
+    }
+
+
+
+
+
+}
