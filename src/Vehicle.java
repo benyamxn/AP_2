ce9ea4c7b9299c2ab75920ea65acd03df726dfc4
mohammadabouei <a.mohammad.abouei@gmail.com>
2018-12-08 15:26:45 +0330

    Vehicle class added

---
+++ src/Vehicle.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public abstract class Vehicle {
+
+    private static final  int A = 30 ;
+    private static final  int B = 30 ;
+    private static final  int C = 30 ;
+    private static final int TIME = 10;
+    private static final int  BASETIME = 70;
+
+    protected ArrayList<ProductType>  contents = new ArrayList<>() ;
+    protected int level = 1;
+    protected int estimatedTimeOfArrival;
+    protected int arrivalTime;
+
+
+    public abstract int getUpgradeCost();
+    public void upgrade(){
+        level++;
+    }
+    public abstract int getProductsPrice();
+
+
+    public int getCapacity() {
+
+        return A * level * level + B * level + C;
+    }
+
+    public int getEstimatedTimeOfArrival(){
+
+        return estimatedTimeOfArrival;
+
+    }
+
+
+    public  void startTravel(){
+        setEstimatedTimeOfArrival(arrivalTime);
+    }
+
+
+    public void setEstimatedTimeOfArrival(int estimatedTimeOfArrival) {
+        this.estimatedTimeOfArrival = estimatedTimeOfArrival;
+    }
+
+    public int getArrivalTime(){
+
+        return BASETIME - level * TIME;
+    }
+
+
+    public void decreaseEstimatedTimeOfArrival(int time){
+
+        this.estimatedTimeOfArrival -= time;
+    }
+
+    public boolean hasArrived(){
+
+        if(estimatedTimeOfArrival <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
