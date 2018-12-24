e1c11622a133e3e8b05b95ee39d743a04d6c1fbc
Aryo Lotfi <aryolotfi77@gmail.com>
2018-12-24 15:33:51 +0330

    Cell class created.

---
+++ src/model/Cell.java
@@ -0,0 +1,140 @@
+package model;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Cell {
+    private Point coordinate;
+    private LinkedList<Animal> animals = new LinkedList<>();
+    private LinkedList<Product> products = new LinkedList<>();
+    private int grassLevel = 0;
+    private static final int GRASS_GROWING_RATE = 1;
+
+    public Cell() {
+    }
+
+    public Cell(Point coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public Point getCoordinate() {
+        return coordinate;
+    }
+
+    public void growGrass() {
+        grassLevel += GRASS_GROWING_RATE;
+    }
+
+    public void eatGrass() {
+        grassLevel--;
+        if (grassLevel < 0)
+            grassLevel = 0;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public ProductType[] updateRemoveProducts() {
+        for (Animal animal : animals) {
+            if (animal instanceof Cat) {
+                return removeProducts();
+            }
+        }
+        Iterator<Product> iterator = products.iterator();
+        while(iterator.hasNext()) {
+            Product temp = iterator.next();
+            temp.decreaseTimeLeftToExpire(1);
+            if(temp.isExpired())
+                iterator.remove();
+            // TODO: check this
+        }
+        return new ProductType[0];
+    }
+
+    public ProductType[] removeProducts() {
+        ProductType[] temp = new ProductType[products.size()];
+        int counter = 0;
+        for (Product product : products) {
+            temp[counter] = product.getType();
+            counter++;
+        }
+        products = new LinkedList<>();
+        return temp;
+    }
+
+    public void updateAddProducts() {
+        for (Animal animal : animals) {
+            if (animal instanceof Domesticated) {
+                Domesticated domesticated = (Domesticated) animal;
+                Product temp = domesticated.produce();
+                if (temp != null) {
+                    products.add(temp);
+                }
+            }
+        }
+    }
+
+    public void updateDomesticatedAnimals() {
+        Iterator<Animal> iterator = animals.iterator();
+        while (iterator.hasNext()) {
+            Animal animal = iterator.next();
+            if (animal instanceof Domesticated) {
+                ((Domesticated) animal).decrementTurnsLeftToProduce();
+                if (grassLevel > 0) {
+                    eatGrass();
+                    ((Domesticated) animal).eat();
+                } else if (((Domesticated) animal).getHealth() < 0) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
+    public void updateWildAnimals() {
+        boolean hasDog = false;
+        Iterator<Animal> iterator = animals.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next() instanceof Dog) {
+                hasDog = true;
+                iterator.remove();
+            }
+        }
+        if (hasDog) {
+            animals.removeIf(animal -> (animal instanceof Wild));
+        }
+    }
+
+    public void cage() {
+        Iterator<Animal> iterator = animals.iterator();
+        while (iterator.hasNext()) {
+            Animal animal = iterator.next();
+            if (animal instanceof Wild) {
+                Wild wild = (Wild) animal;
+                wild.incrementCagedLevel();
+                if (wild.isCaged()) {
+                    iterator.remove();
+                    products.add(new Product(wild.returnCagedProduct()));
+                }
+            }
+        }
+
+    }
+
+    public boolean hasAWildAnimal() {
+        for (Animal animal : animals) {
+            if (animal instanceof Wild)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean hasACatCollectable() {
+        for (Product product : products) {
+            if(!product.getType().name().startsWith("CASED"))
+                return true;
+        }
+        return false;
+    }
+
+}
