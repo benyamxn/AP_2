33fb15cfeaac9b6a927497df450b3981e1baf7a5
Aryo Lotfi <aryolotfi77@gmail.com>
2018-12-24 21:12:32 +0330

    added WorkshopTest.java which tests loading and saving.

---
+++ test/WorkshopTest.java
@@ -0,0 +1,26 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import model.Workshop;
+import model.WorkshopType;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
+
+public class WorkshopTest {
+    @Test
+    public void testJson() {
+        Workshop workshop = new Workshop(WorkshopType.EGG_POWERDER_PLANT);
+        workshop.upgrade();
+        try {
+            workshop.saveToJson("test.json");
+            Gson gson = new GsonBuilder().create();
+            Workshop newWorkshop = Workshop.readFromJson("test.json");
+            assertEquals(workshop, newWorkshop);
+        } catch (IOException e) {
+            fail();
+        }
+    }
+}
