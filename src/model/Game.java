f9108ee986c9265167c4d5f437484472dac1df1a
mohammadabouei <a.mohammad.abouei@gmail.com>
2018-12-26 13:15:30 +0330

    added Well and updateGame to Game

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,5 +1,10 @@
 package model;
 
+import model.exception.MoneyNotEnoughException;
+import model.exception.NotEnoughCapacityException;
+import model.exception.VehicleOnTripException;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,15 +16,37 @@ public class Game {
 
     private Map<ProductType,Integer> products = new HashMap<>();
     private Farm farm = new Farm();
-    private String playerName;
+    private String playerName = "Guest";
 
-    Game(Misson misson ,String playerName){
+    public Game(Misson misson) {
+        this.misson = misson;
+    }
+    public Game(Misson misson , String playerName){
         this.misson = misson;
         this.playerName = playerName;
     }
 
     public void updateGame(){
-            //TODO
+        //TODO set time
+        farm.updateFarm();
+        Truck truck = farm.getTruck();
+        if(truck.isOnTravel()){
+            if(truck.hasArrived()){
+                money += truck.getProductsPrice();
+                truck.finishTravel();
+            }
+        }
+        ArrayList<Product> newProducts = null;
+        Helicopter helicopter = farm.getHelicopter();
+        if(helicopter.isOnTravel()){
+            if(helicopter.hasArrived()){
+               newProducts = helicopter.receiveProducts();
+               helicopter.finishTravel();
+            }
+        }
+        if(newProducts != null){
+            farm.placeProduct(newProducts);
+        }
 
     }
 
@@ -31,8 +58,43 @@ public class Game {
         return false;
     }
 
-    public void well(){
-        //TODO
+    public void well() throws MoneyNotEnoughException {
+        if( money >= farm.getWell().getRefillPrice()) {
+            money -= farm.getWell().getRefillPrice();
+            farm.getWell().refill();
+        }
+        else{
+            throw new MoneyNotEnoughException();
+        }
+    }
+
+    public void moveVehicle(Vehicle vehicle,HashMap<ProductType,Integer> products) throws VehicleOnTripException,NotEnoughCapacityException,MoneyNotEnoughException {
+
+        if(vehicle.onTravel){
+            throw new VehicleOnTripException();
+        }
+        else{
+            double capacity = 0;
+            for (Map.Entry<ProductType, Integer> product : products.entrySet()) {
+               capacity += product.getKey().getDepotSize() * product.getValue();
+            }
+            if(capacity > vehicle.getCapacity()){
+                throw new NotEnoughCapacityException();
+            }
+            else{
+                if(vehicle instanceof Helicopter){
+                    Helicopter temp  = new Helicopter();
+                    temp.startTravel(products);
+                    if(temp.getProductsPrice() > money){
+                        throw new MoneyNotEnoughException();
+                    }
+                    else{
+                        money -= temp.getProductsPrice();
+                    }
+                }
+                vehicle.startTravel(products);
+            }
+        }
 
     }
 
