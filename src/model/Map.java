438cb132d98ff4d64c8b6e10ad52261c596ad23c
mohammadabouei <a.mohammad.abouei@gmail.com>
2018-12-27 18:46:57 +0330

    fixed a bug in Map.

--- src/model/Map.java
+++ src/model/Map.java
@@ -16,6 +16,7 @@ public class Map {
     public Map(int width, int height) {
         this.width = width;
         this.height = height;
+        cells = new Cell[width][height];
         constructCells();
     }
 
