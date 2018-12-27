5095a0aa851b43e83ec5a81c3bd61a95e8ae7e29
mohammadabouei <a.mohammad.abouei@gmail.com>
2018-12-27 17:09:20 +0330

    added MaxLevelException.

---
+++ src/model/exception/MaxLevelException.java
@@ -0,0 +1,9 @@
+package model.exception;
+
+public class MaxLevelException extends Exception {
+    public MaxLevelException() {
+    }
+    public MaxLevelException(String s){
+        super(s);
+    }
+}
