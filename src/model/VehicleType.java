3a89fc7974f024867ffbbd63c6e36295b93c3fcf
Benyamin Ghaseminia <bengbenji@gmail.com>
2019-01-20 21:55:56 +0330

    overrided toString method in VehicleType

--- src/model/VehicleType.java
+++ src/model/VehicleType.java
@@ -2,4 +2,17 @@ package model;
 
 public enum VehicleType {
     HELICOPTER, TRUCK;
+
+
+    @Override
+    public String toString() {
+        switch (this){
+
+            case HELICOPTER:
+                return "Helicopter";
+            case TRUCK:
+                return "Truck";
+        }
+        return "";
+    }
 }
